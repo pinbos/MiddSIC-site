dace9100ac0b9e4cc555f22ab8eca1746b879401
Matthew N <nannis.matt@gmail.com>
2025-06-21 21:50:10 -0400

    FIX: Add microsoft_auth to INSTALLED_APPS

--- MiddSIC_site/settings.py
+++ MiddSIC_site/settings.py
@@ -41,7 +41,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'django.contrib.sites',          # Required by allauth
-
+    'microsoft_auth',
     'allauth',
     'allauth.account',
     'allauth.socialaccount',
